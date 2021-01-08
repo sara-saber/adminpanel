2bb67804a9b08af3a8f016635a72a512ac576984
sarah <sarah.sa.1889@gmail.com>
2021-01-08 22:32:30 +0300

    Sara

--- Admin/Controllers/HomeController.cs
+++ Admin/Controllers/HomeController.cs
@@ -20,11 +20,54 @@ namespace Admin.Controllers
 
         public IActionResult Index()
         {
+            ViewData["class_1"] = "bg-info";
+            ViewData["number_1"] = "150";
+            ViewData["name_1"] = "New Orders";
+            ViewData["icon_1"] = "ion-bag";
+
+            ViewData["class_2"] = "bg-success";
+            ViewData["number_2"] = "53";
+            ViewData["name_2"] = "Bounce Rate";
+            ViewData["icon_2"] = "ion-stats-bars";
+
+            ViewData["class_3"] = "bg-warning";
+            ViewData["number_3"] = "44";
+            ViewData["name_3"] = "User Registrations";
+            ViewData["icon_3"] = "ion-person-add";
+
+
+            ViewData["class_4"] = "bg-danger";
+            ViewData["number_4"] = "65";
+            ViewData["name_4"] = "Unique Visitors";
+            ViewData["icon_4"] = "ion-pie-graph";
             return View();
         }
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+        public IActionResult Index1()
+        {
+            ViewData["class_1"] = "bg-info";
+            ViewData["number_1"] = "10";
+            ViewData["text_1"] = "CPU Traffic";
+            ViewData["icon_1"] = "fas fa-cog";
+
+            ViewData["class_2"] = "bg-danger";
+            ViewData["number_2"] = "41,410";
+            ViewData["text_2"] = "Likes";
+            ViewData["icon_2"] = "fas fa-thumbs-up";
+
+            ViewData["class_3"] = " bg-success";
+            ViewData["number_3"] = "760";
+            ViewData["text_3"] = "Sales";
+            ViewData["icon_3"] = "fas fa-shopping-cart";
+
+            ViewData["class_4"] = "bg-warning";
+            ViewData["number_4"] = "2,000";
+            ViewData["text_4"] = "New Members";
+            ViewData["icon_4"] = "fas fa-users";
             return View();
         }
 
